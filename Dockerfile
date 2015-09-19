46eacf3c8e5133b21f73069b4d2228c8fc400233
Ryan Mitchell <ryan.mitchell@troux.com>
2015-09-19 11:20:05 -0500

    latest updates to dockerfile and shell script to support environment variable of minecraft jar

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,10 @@ RUN su -c "yum install -y java-1.8.0-openjdk"
 # Expose minecraft port
 EXPOSE 25565
 
+# VOLUME /minecraft
 ADD . /minecraft
 WORKDIR /minecraft
+
+ENV MINECRAFT_JAR_URL=1.8.8/minecraft_server.1.8.8.jar
 
 CMD ./buildMinecraftServer.sh && ./runMinecraftServer.sh
--- buildMinecraftServer.sh
+++ buildMinecraftServer.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Set the Minecraft Jar URL if not otherwise set
+: ${MINECRAFT_JAR_URL:=1.8.8/minecraft_server.1.8.8.jar}
+
 mkdir minecraft-server
 cd minecraft-server
-wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8.8/minecraft_server.1.8.8.jar
+wget https://s3.amazonaws.com/Minecraft.Download/versions/$MINECRAFT_JAR_URL
 
 echo "eula=true" > eula.txt
