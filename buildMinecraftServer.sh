1f4817c57b03682f37d3feb36cde180ce669748b
Ryan Mitchell <ryan.mitchell@troux.com>
2015-09-27 14:40:46 -0500

    added support for minecraft server variable to pull in server template

--- buildMinecraftServer.sh
+++ buildMinecraftServer.sh
@@ -4,6 +4,17 @@
 : ${MINECRAFT_JAR_URL:=1.8.8/minecraft_server.1.8.8.jar}
 
 mkdir minecraft-server
+
+# Copy the ops.json file if it exists
+if [ -f server-settings/ops.json ]; then
+  cp server-settings/ops.json minecraft-server
+fi
+
+# Copy a template file if defined by env var
+if [ -n "$MINECRAFT_SERVER_PROPERTIES" ]; then
+  cp server-settings/server-properties-templates/$MINECRAFT_SERVER_PROPERTIES.properties minecraft-server/server.properties
+fi
+
 cd minecraft-server
 wget https://s3.amazonaws.com/Minecraft.Download/versions/$MINECRAFT_JAR_URL
 
