b72b939a89ee9b319e0cc30551cf30ae5bb96bdb
Ryan Mitchell <mitch@threeweekhobby.com>
2018-09-02 14:54:44 -0500

    wip: renamed scripts

---
+++ gcloud/provision-local.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+sudo apt-get update
+sudo apt-get install -y tmux wget curl git gcc htop
+git clone https://github.com/newtmitch/minecraft-server.git
+cd minecraft-server
+./buildMinecraftServer.sh
+
+# run the minecraft server
+tmux new-session -s "mc-server" ./runMinecraftServer.sh
